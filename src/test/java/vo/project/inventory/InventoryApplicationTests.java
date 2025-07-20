b32a09ef8d363de4e439654952dda22ac74e3baa
Sophia Portela <sophianobreportela@gmail.com>
2025-07-20 19:47:30 -0300

    Initial commit

---
+++ src/test/java/vo/project/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package vo.project.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
