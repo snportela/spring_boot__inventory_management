59bcc2ebff01c30afeadcd32e4ab6f75cf5b942a
Sophia Portela <sophianobreportela@gmail.com>
2025-07-21 20:26:26 -0300

    feat: update areas table migration

--- src/main/resources/db/migration/V01__create-areas-table.sql
+++ src/main/resources/db/migration/V01__create-areas-table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE areas (
     area_id uuid DEFAULT gen_random_uuid() NOT NULL,
     name varchar(100) NOT NULL,
-    created_at timestamp DEFAULT NOW(),
-    updated_at timestamp DEFAULT NOW(),
-    deleted_at timestamp NULL,
+    created_at TIMESTAMP DEFAULT TIMEZONE('UTC', NOW()),
+    updated_at TIMESTAMP DEFAULT TIMEZONE('UTC', NOW()),
+    deleted_at TIMESTAMP,
 
     CONSTRAINT areas_pk PRIMARY KEY (area_id)
 );
