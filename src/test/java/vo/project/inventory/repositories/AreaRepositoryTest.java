85041f2fa23b1b91a946edbf7e963f14a1fc3294
Sophia Portela <sophianobreportela@gmail.com>
2025-08-01 18:27:34 -0300

    feat: update tests

--- src/test/java/vo/project/inventory/repositories/AreaRepositoryTest.java
+++ src/test/java/vo/project/inventory/repositories/AreaRepositoryTest.java
@@ -3,17 +3,24 @@ package vo.project.inventory.repositories;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.test.context.TestPropertySource;
 import vo.project.inventory.domain.Area;
-import vo.project.inventory.util.AreaData;
+import vo.project.inventory.specifications.AreaSpec;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ActiveProfiles("test")
-@SpringBootTest
+@TestPropertySource(
+        locations = "classpath:application-test.properties")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class AreaRepositoryTest {
 
     @Autowired
@@ -25,8 +32,8 @@ public class AreaRepositoryTest {
     }
 
     @Test
-    void testThatAreaCanBeCreatedAndRecalled() {
-        Area newArea = AreaData.createTestAreaEntityA();
+    void AreaRepository_SaveArea_ReturnSavedArea() {
+        Area newArea = Area.builder().name("engenharia").build();
         areaRepository.save(newArea);
         Optional<Area> result = areaRepository.findById(newArea.getAreaId());
         assertThat(result).isPresent();
@@ -34,21 +41,23 @@ public class AreaRepositoryTest {
     }
 
     @Test
-    void testThatMultipleAreasCanBeCreatedAndRecalled() {
-        Area areaA = AreaData.createTestAreaEntityA();
+    void AreaRepository_FindAll_ReturnPaginatedItems() {
+        Area areaA = Area.builder().name("engenharia").build();
         areaRepository.save(areaA);
-        Area areaB = AreaData.createTestAreaEntityB();
+        Area areaB = Area.builder().name("medicina").build();
         areaRepository.save(areaB);
 
-        Iterable<Area> result = areaRepository.findAll();
-        assertThat(result)
-                .hasSize(2)
-                .containsExactly(areaA, areaB);
+        Pageable pageable = PageRequest.of(0, 2);
+        Specification<Area> spec = AreaSpec.nameContains("medic");
+
+        Page<Area> page = areaRepository.findAll(spec, pageable);
+        assertThat(page.getContent()).hasSize(1)
+                .containsExactly(areaB);
     }
 
     @Test
-    void testThatAreaCanBeUpdated() {
-        Area areaA = AreaData.createTestAreaEntityA();
+    void AreaRepository_Update_ReturnArea() {
+        Area areaA = Area.builder().name("engenharia").build();
         areaRepository.save(areaA);
         areaA.setName("updated");
         areaRepository.save(areaA);
@@ -58,8 +67,8 @@ public class AreaRepositoryTest {
     }
 
     @Test
-    void testThatAreaCanBeDeleted() {
-        Area areaA = AreaData.createTestAreaEntityA();
+    void AreaRepository_Delete_ReturnAreaIsEmpty() {
+        Area areaA = Area.builder().name("engenharia").build();
         areaRepository.save(areaA);
         areaRepository.deleteById(areaA.getAreaId());
         Optional<Area> result = areaRepository.findById(areaA.getAreaId());
--- src/test/java/vo/project/inventory/services/AreaServiceTest.java
+++ src/test/java/vo/project/inventory/services/AreaServiceTest.java
@@ -5,15 +5,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import vo.project.inventory.domain.Area;
 import vo.project.inventory.exceptions.NotFoundException;
 import vo.project.inventory.repositories.AreaRepository;
 import vo.project.inventory.services.impl.AreaServiceImpl;
-import vo.project.inventory.util.AreaData;
+import vo.project.inventory.specifications.AreaSpec;
 
 import java.util.*;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -28,39 +33,40 @@ public class AreaServiceTest {
     private AreaServiceImpl areaService;
 
     @Test
-    void testThatAreaCanBeCreated() {
-        Area areaA = AreaData.createTestAreaEntityA();
-        when(areaRepository.save(any(Area.class))).thenReturn(areaA);
-        Area newArea = areaService.save(areaA);
+    void AreaService_CreateArea_ReturnCreated() {
+        Area area = Area.builder().name("engenharia").build();
+        when(areaRepository.save(any(Area.class))).thenReturn(area);
+        Area newArea = areaService.save(area);
 
-        assertEquals("engenharia", newArea.getName());
+        assertThat(newArea).isNotNull();
+
         verify(areaRepository).save(any(Area.class));
     }
 
     @Test
-    void testThatMultipleAreasCanBeCreatedAndRecalled() {
+    void AreaService_FindAll_ReturnPaginatedItems() {
         List<Area> list = new ArrayList<>();
-        list.add(AreaData.createTestAreaEntityA());
-        list.add(AreaData.createTestAreaEntityB());
+        list.add(Area.builder().name("engenharia").build());
+        list.add(Area.builder().name("medicina").build());
 
         Pageable pageable = PageRequest.of(0, 10);
+        Specification<Area> spec = AreaSpec.nameContains("");
 
-        when(areaRepository.findAll(pageable))
+        when(areaRepository.findAll(spec, pageable))
                 .thenReturn(new PageImpl<>(list, PageRequest.of(0, 10), list.size()));
 
-        Page<Area> areas = areaService.findAll(pageable);
+        Page<Area> areas = areaService.findAll(spec, pageable);
 
         assertEquals(2, areas.getTotalElements());
         assertNotNull(areas);
     }
 
     @Test
-    void testThatAreaCanBeRecalledById() {
+    void AreaService_GetArea_ReturnArea() {
         UUID areaId = UUID.fromString("a1b2c3d4-e5f6-7890-1234-567890abcdef");
-        Area areaA = AreaData.createTestAreaEntityA();
-        areaA.setAreaId(areaId);
+        Area area = Area.builder().areaId(areaId).name("engenharia").build();
 
-        when(areaRepository.findById(areaId)).thenReturn(Optional.of(areaA));
+        when(areaRepository.findById(areaId)).thenReturn(Optional.of(area));
         Area existingArea = areaService.findOne(areaId);
 
         assertNotNull(existingArea);
@@ -68,7 +74,7 @@ public class AreaServiceTest {
     }
 
     @Test
-    void testThatExceptionShouldBeThrownWhenAreaNotFound() {
+    void AreaService_GetAreaIsEmpty_ThrowsNotFound() {
         UUID areaId = UUID.randomUUID();
 
         when(areaRepository.findById(areaId)).thenReturn(Optional.empty());
@@ -76,28 +82,27 @@ public class AreaServiceTest {
     }
 
     @Test
-    void testThatAreaCanBeUpdated() {
+    void AreaService_UpdateArea_ReturnArea() {
         UUID areaId = UUID.fromString("a1b2c3d4-e5f6-7890-1234-567890abcdef");
-        Area areaA = AreaData.createTestAreaEntityA();
-        areaA.setAreaId(areaId);
+        Area area = Area.builder().areaId(areaId).name("engenharia").build();
 
-        when(areaRepository.findById(areaId)).thenReturn(Optional.of(areaA));
-        when(areaRepository.save(areaA)).thenReturn(areaA);
+        when(areaRepository.findById(areaId)).thenReturn(Optional.of(area));
+        when(areaRepository.save(area)).thenReturn(area);
 
-        areaA.setName("design");
+        area.setName("design");
 
-        Area result = areaService.update(areaId, areaA);
+        Area result = areaService.update(areaId, area);
 
         assertNotNull(result);
         assertEquals("design", result.getName());
     }
 
     @Test
-    void testThatAreaCanBeDeleted() {
+    void AreaService_DeleteArea_ReturnVoid() {
         UUID areaId = UUID.fromString("a1b2c3d4-e5f6-7890-1234-567890abcdef");
-        Area areaA = AreaData.createTestAreaEntityA();
+        Area area = Area.builder().areaId(areaId).name("engenharia").build();
 
-        when(areaRepository.findById(areaId)).thenReturn(Optional.of(areaA));
+        when(areaRepository.findById(areaId)).thenReturn(Optional.of(area));
         doNothing().when(areaRepository).deleteById(areaId);
 
         assertAll(() -> areaService.delete(areaId));
