2bd06ab48f8496b7c53233bf7b7960d7c3f88573
Sophia Portela <sophianobreportela@gmail.com>
2025-08-01 18:30:38 -0300

    feat: implement validation exception

--- src/main/java/vo/project/inventory/exceptions/GlobalExceptionHandler.java
+++ src/main/java/vo/project/inventory/exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,9 @@
 package vo.project.inventory.exceptions;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,10 +12,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
