de5d21dc6e08b83946f7c8e94c284547591ebf2e
Sophia Portela <sophianobreportela@gmail.com>
2025-08-02 01:12:11 -0300

    feat: create category repository

---
+++ src/main/java/vo/project/inventory/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package vo.project.inventory.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import vo.project.inventory.domain.Category;
+
+import java.util.UUID;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID>, JpaSpecificationExecutor<Category> {
+}
