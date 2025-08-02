8d0388c7b4cd65a06478d70cfe71051ba907213d
Sophia Portela <sophianobreportela@gmail.com>
2025-08-02 01:14:22 -0300

    fix: update areas dto

--- src/main/java/vo/project/inventory/dtos/AreaDto.java
+++ src/main/java/vo/project/inventory/dtos/AreaDto.java
@@ -6,11 +6,10 @@ import jakarta.validation.constraints.Size;
 import java.util.UUID;
 
 public record AreaDto(
-
         UUID areaId,
 
-        @NotBlank(message = "Name is required")
-        @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters long")
+        @NotBlank(message = "Area name is required")
+        @Size(min = 2, max = 100, message = "Area name must be between 2 and 100 characters long")
         String name
 ) {
 }
