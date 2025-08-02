d7853b2109baa33e5bf63a80a239702d99996525
Sophia Portela <sophianobreportela@gmail.com>
2025-08-02 01:14:34 -0300

    fix: update areas repository test

--- src/test/java/vo/project/inventory/repositories/AreaRepositoryTest.java
+++ src/test/java/vo/project/inventory/repositories/AreaRepositoryTest.java
@@ -1,6 +1,5 @@
 package vo.project.inventory.repositories;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -9,7 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.test.context.TestPropertySource;
 import vo.project.inventory.domain.Area;
 import vo.project.inventory.specifications.AreaSpec;
 
@@ -17,19 +15,12 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@TestPropertySource(
-        locations = "classpath:application-test.properties")
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class AreaRepositoryTest {
 
     @Autowired
     private AreaRepository areaRepository;
-
-    @BeforeEach
-    void init() {
-        areaRepository.deleteAll();
-    }
 
     @Test
     void AreaRepository_SaveArea_ReturnSavedArea() {
@@ -53,6 +44,14 @@ public class AreaRepositoryTest {
         Page<Area> page = areaRepository.findAll(spec, pageable);
         assertThat(page.getContent()).hasSize(1)
                 .containsExactly(areaB);
+    }
+
+    @Test
+    void AreaRepository_FindById_ReturnArea() {
+        Area newArea = Area.builder().name("engenharia").build();
+        areaRepository.save(newArea);
+        Optional<Area> result = areaRepository.findById(newArea.getAreaId());
+        assertThat(result).isNotNull();
     }
 
     @Test
