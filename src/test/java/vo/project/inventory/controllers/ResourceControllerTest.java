99d36576b24513fc2ea6a7fc03d87ecedd72bd59
Sophia Portela <sophianobreportela@gmail.com>
2025-08-08 00:49:04 -0300

    feat: create Resource tests

---
+++ src/test/java/vo/project/inventory/controllers/ResourceControllerTest.java
@@ -0,0 +1,180 @@
+package vo.project.inventory.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import vo.project.inventory.InventoryApplication;
+import vo.project.inventory.domain.Area;
+import vo.project.inventory.domain.Category;
+import vo.project.inventory.domain.Resource;
+import vo.project.inventory.domain.enums.RepairState;
+import vo.project.inventory.domain.enums.Status;
+import vo.project.inventory.domain.enums.UseTime;
+import vo.project.inventory.services.AreaService;
+import vo.project.inventory.services.CategoryService;
+import vo.project.inventory.services.ResourceService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = InventoryApplication.class)
+@AutoConfigureMockMvc
+public class ResourceControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private AreaService areaService;
+
+    @MockitoBean
+    private CategoryService categoryService;
+
+    @MockitoBean
+    ResourceService resourceService;
+
+    Resource resource = new Resource();
+    Area area = new Area();
+    Category category = new Category();
+    UUID resourceId = UUID.randomUUID();
+
+
+    @BeforeEach
+    void setup() {
+        area = Area.builder().name("engenharia").build();
+        areaService.save(area);
+
+        category = Category.builder().name("computador").build();
+        categoryService.save(category);
+
+        resource = Resource.builder()
+                .area(area)
+                .category(category)
+                .name("PC")
+                .description("pc windows")
+                .manufactureYear("n/a")
+                .serialNumber("N/A")
+                .repairState(RepairState.OPERATIONAL)
+                .resourceNumber("324343")
+                .status(Status.BEING_USED)
+                .observation("aaaa")
+                .useTime(UseTime.LESS_THAN_ONE_YEAR)
+                .build();
+    }
+
+    @Test
+    void ResourceController_CreateResource_ReturnCreated() throws Exception {
+
+        when(resourceService.save(any(Resource.class))).thenReturn(resource);
+
+        this.mockMvc.perform(post("/api/resources")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(resource))
+        )
+                .andExpect(status().isCreated())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", CoreMatchers.is(resource.getName())));
+
+        verify(resourceService,times(1)).save(any(Resource.class));
+    }
+
+    @Test
+    void ResourceController_CreateInvalidResource_ReturnCorrectResponse() throws Exception {
+        resource.setName("");
+        when(resourceService.save(any(Resource.class))).thenReturn(resource);
+
+        this.mockMvc.perform(post("/api/resources")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(resource)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", CoreMatchers.is("{name=Resource name must be between 2 and 100 characters long}")));
+    }
+
+    @Test
+    void ResourceController_ListResources_ReturnPaginatedItems() throws Exception {
+        List<Resource> list = new ArrayList<>();
+        list.add(resource);
+
+        when(resourceService.findAll(Mockito.any(), any(Pageable.class)))
+                .thenReturn(new PageImpl<>(list, PageRequest.of(0,10), list.size()));
+
+        this.mockMvc.perform(get("/api/resources")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("page", "1")
+                .param("size", "10")
+                .param("sort", "name,asc"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalItems", CoreMatchers.is(list.size())));
+
+        verify(resourceService, times(1)).findAll(Mockito.any(), any(Pageable.class));
+
+    }
+
+    @Test
+    void ResourceController_GetResource_ReturnCategory() throws Exception {
+        when(resourceService.findOne(resourceId)).thenReturn(resource);
+
+        this.mockMvc.perform(get("/api/resources/" + resourceId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(resource)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", CoreMatchers.is(resource.getName())));
+
+        verify(resourceService,times(1)).findOne(resourceId);
+    }
+
+    @Test
+    void ResourceController_UpdateResource_ReturnResource() throws Exception {
+        when(resourceService.update(resourceId, resource)).thenReturn(resource);
+
+        this.mockMvc.perform(put("/api/resources/" + resourceId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(resource)))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", CoreMatchers.is(resource.getName())));
+
+        verify(resourceService, times(1)).update(resourceId, resource);
+    }
+
+    @Test
+    void ResourceController_DeleteResource_ReturnString() throws Exception {
+        doNothing().when(resourceService).delete(resourceId);
+
+        this.mockMvc.perform(delete("/api/resources/" + resourceId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        verify(resourceService, times(1)).delete(resourceId);
+    }
+
+}
---
+++ src/test/java/vo/project/inventory/services/ResourceServiceTest.java
@@ -0,0 +1,159 @@
+package vo.project.inventory.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import vo.project.inventory.domain.Area;
+import vo.project.inventory.domain.Category;
+import vo.project.inventory.domain.Resource;
+import vo.project.inventory.domain.enums.RepairState;
+import vo.project.inventory.domain.enums.Status;
+import vo.project.inventory.domain.enums.UseTime;
+import vo.project.inventory.exceptions.NotFoundException;
+import vo.project.inventory.repositories.AreaRepository;
+import vo.project.inventory.repositories.CategoryRepository;
+import vo.project.inventory.repositories.ResourceRepository;
+import vo.project.inventory.services.impl.AreaServiceImpl;
+import vo.project.inventory.services.impl.CategoryServiceImpl;
+import vo.project.inventory.services.impl.ResourceServiceImpl;
+import vo.project.inventory.specifications.ResourceSpec;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ResourceServiceTest {
+
+    @Mock
+    private ResourceRepository resourceRepository;
+
+    @Mock
+    private AreaRepository areaRepository;
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @InjectMocks
+    private AreaServiceImpl areaService;
+
+    @InjectMocks
+    private CategoryServiceImpl categoryService;
+
+    @InjectMocks
+    private ResourceServiceImpl resourceService;
+
+    Resource resource = new Resource();
+    Area area = new Area();
+    Category category = new Category();
+    UUID resourceId = UUID.randomUUID();
+
+    @BeforeEach
+    void setup() {
+        area = Area.builder().name("engenharia").build();
+        areaService.save(area);
+
+        category = Category.builder().name("computador").build();
+        categoryService.save(category);
+
+        resource = Resource.builder()
+                .area(area)
+                .category(category)
+                .name("PC")
+                .description("pc windows")
+                .manufactureYear("2012")
+                .serialNumber("N/A")
+                .repairState(RepairState.OPERATIONAL)
+                .resourceNumber("324343")
+                .status(Status.BEING_USED)
+                .observation("aaaa")
+                .useTime(UseTime.LESS_THAN_ONE_YEAR)
+                .build();
+    }
+
+    @Test
+    void ResourceService_CreateResource_ReturnCreated() {
+        when(resourceRepository.save(any(Resource.class))).thenReturn(resource);
+
+        Resource newResource = resourceService.save(resource);
+
+        assertThat(newResource).isNotNull();
+
+        verify(resourceRepository).save(any(Resource.class));
+    }
+
+    @Test
+    void ResourceService_FindAll_ReturnPaginatedItems() {
+        List<Resource> list = new ArrayList<>();
+        list.add(resource);
+
+        Pageable pageable = PageRequest.of(0, 2);
+        Specification<Resource> spec = ResourceSpec
+                .nameContains("PC")
+                .and(ResourceSpec.repairStateEquals(RepairState.OPERATIONAL))
+                .and(ResourceSpec.categoryNameContains(category.getName()));
+
+        when(resourceRepository.findAll(spec, pageable))
+                .thenReturn(new PageImpl<>(list, PageRequest.of(0,10), list.size()));
+
+        Page<Resource> resources = resourceService.findAll(spec, pageable);
+
+        assertEquals(1, resources.getTotalElements());
+        assertNotNull(resources);
+
+    }
+
+    @Test
+    void ResourceService_GetResource_ReturnResource() {
+        resource.setResourceId(resourceId);
+
+        when(resourceRepository.findById(resourceId)).thenReturn(Optional.of(resource));
+        Resource existingResource = resourceService.findOne(resourceId);
+
+        assertNotNull(existingResource);
+        assertEquals(resourceId, existingResource.getResourceId());
+    }
+
+    @Test
+    void ResourceService_GetResourceIsEmpty_ThrowsNotFound() {
+        when(resourceRepository.findById(resourceId)).thenReturn(Optional.empty());
+        assertThrows(NotFoundException.class, () -> resourceService.findOne(resourceId));
+    }
+
+    @Test
+    void ResourceService_UpdateService_ReturnResource() {
+        when(resourceRepository.findById(resourceId)).thenReturn(Optional.of(resource));
+        when(resourceRepository.save(resource)).thenReturn(resource);
+
+        resource.setName("updated");
+
+        Resource result = resourceService.update(resourceId, resource);
+
+        assertNotNull(result);
+        assertEquals("updated", result.getName());
+    }
+
+    @Test
+    void ResourceService_DeleteResource_ReturnVoid() {
+        resource.setResourceId(resourceId);
+
+        when(resourceRepository.findById(resourceId)).thenReturn(Optional.of(resource));
+        doNothing().when(resourceRepository).deleteById(resourceId);
+
+        assertAll(()-> resourceService.delete(resourceId));
+    }
+}
