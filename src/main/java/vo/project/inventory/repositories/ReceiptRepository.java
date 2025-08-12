fbec7fb17988ff6fc249765d83b7cf65ff1c8610
Sophia Portela <sophianobreportela@gmail.com>
2025-08-12 16:49:34 -0300

    feat: create Receipts repository

---
+++ src/main/java/vo/project/inventory/repositories/ReceiptRepository.java
@@ -0,0 +1,10 @@
+package vo.project.inventory.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import vo.project.inventory.domain.Receipt;
+
+import java.util.UUID;
+
+public interface ReceiptRepository extends JpaRepository<Receipt, UUID>, JpaSpecificationExecutor<Receipt> {
+}
