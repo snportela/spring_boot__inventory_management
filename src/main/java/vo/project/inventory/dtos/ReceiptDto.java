48e80a8d330a45e8c70fcaae1404124b82f4c8b3
Sophia Portela <sophianobreportela@gmail.com>
2025-08-12 16:48:53 -0300

    feat: create Receipts dto

---
+++ src/main/java/vo/project/inventory/dtos/ReceiptDto.java
@@ -0,0 +1,28 @@
+package vo.project.inventory.dtos;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+public record ReceiptDto(
+
+        UUID receiptId,
+
+        @NotNull(message = "Receipt Number is required")
+        @Size(min = 2, max = 100, message = "Receipt Number must be between 2 and 100 characters long")
+        String receiptNumber,
+
+        @NotNull(message = "Receipt Price is required")
+        BigDecimal price,
+
+        @NotNull(message = "Receipt Supplier is required")
+        @Size(min = 2, max = 100, message = "Receipt Supplier must be between 2 and 100 characters long")
+        String supplier,
+
+        @NotNull(message = "Receipt Date is required")
+        Instant receiptDate
+) {
+}
