9f9e62756f0a4245f273c752d0a6174979653169
Sophia Portela <sophianobreportela@gmail.com>
2025-08-20 00:43:29 -0300

    feat: create Loan entity

---
+++ src/main/java/vo/project/inventory/domain/Loan.java
@@ -0,0 +1,57 @@
+package vo.project.inventory.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.UpdateTimestamp;
+import vo.project.inventory.domain.enums.LoanStatus;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@SQLDelete(sql = "UPDATE loans SET deleted_at = NOW() WHERE loan_id = ?")
+@SQLRestriction(value = "deleted_at IS NULL")
+@Table(name= "loans")
+public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID loanId;
+
+    @Column(name = "student_name")
+    private String studentName;
+
+    @Column(name = "student_id")
+    private String studentId;
+
+    @Column(name = "loan_date")
+    private Instant loanDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "loan_status")
+    private LoanStatus loanStatus;
+
+    private String observation;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "deleted_at")
+    private Instant deletedAt;
+
+}
