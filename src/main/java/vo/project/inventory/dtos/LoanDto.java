a939c5af35d83c92abf3148da8e5481016b5d1ba
Sophia Portela <sophianobreportela@gmail.com>
2025-08-20 13:34:16 -0300

    feat: create Loan Dto

---
+++ src/main/java/vo/project/inventory/dtos/LoanDto.java
@@ -0,0 +1,29 @@
+package vo.project.inventory.dtos;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import vo.project.inventory.domain.enums.LoanStatus;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record LoanDto(
+
+        UUID loanId,
+
+        @NotNull(message = "Student Name is required")
+        @Size(min = 2, max = 100, message = "Student Name must be between 2 and 100 characters long")
+        String studentName,
+
+        @NotNull(message = "Student Id is required")
+        String studentId,
+
+        @NotNull(message = "Loan Date is required")
+        Instant loanDate,
+
+        @NotNull(message = "Loan Status is required")
+        LoanStatus loanStatus,
+
+        String observation
+) {
+}
