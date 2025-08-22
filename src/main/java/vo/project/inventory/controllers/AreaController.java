ac4e09e9c6a1765d204423c4747dc455ce78e594
Sophia Portela <sophianobreportela@gmail.com>
2025-08-22 20:21:27 -0300

    feat: update new service methods

--- src/main/java/vo/project/inventory/controllers/AreaController.java
+++ src/main/java/vo/project/inventory/controllers/AreaController.java
@@ -1,21 +1,16 @@
 package vo.project.inventory.controllers;
 
 import jakarta.validation.Valid;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import vo.project.inventory.domain.Area;
 import vo.project.inventory.dtos.AreaDto;
-import vo.project.inventory.mappers.AreaMapper;
 import vo.project.inventory.services.AreaService;
 import vo.project.inventory.specifications.AreaSpec;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/areas")
@@ -23,47 +18,36 @@ public class AreaController {
 
     private final AreaService areaService;
 
-    private final AreaMapper areaMapper;
-
-    public AreaController(AreaService areaService, AreaMapper areaMapper) {
+    public AreaController(AreaService areaService) {
         this.areaService = areaService;
-        this.areaMapper = areaMapper;
     }
 
     @GetMapping
     public ResponseEntity<Map<String, Object>> listAreas(
             @RequestParam(required = false) String name,
             Pageable pageable) {
-        Page<Area> areaList = areaService.findAll(AreaSpec.nameContains(name), pageable);
-
-        Map<String, Object> response = new HashMap<>();
-
-        response.put("areas", areaList.getContent().stream().map(areaMapper::areaToDto).collect(Collectors.toList()));
-        response.put("currentPage", areaList.getNumber());
-        response.put("totalItems", areaList.getTotalElements());
-        response.put("totalPages", areaList.getTotalPages());
-
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        Map<String, Object> areaList = areaService.findAll(AreaSpec.nameContains(name), pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(areaList);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<AreaDto> getArea(@PathVariable("id") UUID areaId) {
-        Area foundArea = areaService.findOne(areaId);
-        return ResponseEntity.status(HttpStatus.OK).body(areaMapper.areaToDto(foundArea));
+        AreaDto foundArea = areaService.findOne(areaId);
+        return ResponseEntity.status(HttpStatus.OK).body(foundArea);
     }
 
     @PostMapping
     public ResponseEntity<AreaDto> createArea(@Valid @RequestBody AreaDto areaDto) {
-        Area savedArea = areaService.save(areaMapper.dtoToArea(areaDto));
-        return ResponseEntity.status(HttpStatus.CREATED).body(areaMapper.areaToDto(savedArea));
+        AreaDto savedArea = areaService.save(areaDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedArea);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<AreaDto> updateArea(
             @PathVariable("id") UUID areaId, @Valid @RequestBody AreaDto areaDto
     ) {
-        Area updatedArea = areaService.update(areaId, areaMapper.dtoToArea(areaDto));
-        return ResponseEntity.status(HttpStatus.CREATED).body(areaMapper.areaToDto(updatedArea));
+        AreaDto updatedArea = areaService.update(areaId, areaDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(updatedArea);
     }
 
     @DeleteMapping("/{id}")
