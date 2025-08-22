31dfc26e9bea81eeddb8472286ea6d8bf1356b12
Sophia Portela <sophianobreportela@gmail.com>
2025-08-22 20:56:29 -0300

    feat: update service methods

--- src/main/java/vo/project/inventory/controllers/ResourceController.java
+++ src/main/java/vo/project/inventory/controllers/ResourceController.java
@@ -1,21 +1,16 @@
 package vo.project.inventory.controllers;
 
 import jakarta.validation.Valid;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import vo.project.inventory.domain.Resource;
 import vo.project.inventory.dtos.ResourceDto;
-import vo.project.inventory.mappers.ResourceMapper;
 import vo.project.inventory.query_filters.ResourceQueryFilter;
 import vo.project.inventory.services.ResourceService;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/resources")
@@ -23,39 +18,28 @@ public class ResourceController {
 
     private final ResourceService resourceService;
 
-    private final ResourceMapper resourceMapper;
-
-    public ResourceController(ResourceService resourceService, ResourceMapper resourceMapper) {
+    public ResourceController(ResourceService resourceService) {
         this.resourceService = resourceService;
-        this.resourceMapper = resourceMapper;
     }
 
     @GetMapping
     public ResponseEntity<Map<String, Object>> listResources(
             ResourceQueryFilter filter, Pageable pageable
     ) {
-        Page<Resource> resourceList = resourceService.findAll(filter.toSpecification(), pageable);
-
-        Map<String, Object> response = new HashMap<>();
-
-        response.put("resources", resourceList.getContent().stream().map(resourceMapper::resourceToDto).collect(Collectors.toList()));
-        response.put("currentPage", resourceList.getNumber());
-        response.put("totalItems", resourceList.getTotalElements());
-        response.put("totalPages", resourceList.getTotalPages());
-
+        Map<String, Object> response = resourceService.findAll(filter.toSpecification(), pageable);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ResourceDto> getResource(@PathVariable("id") UUID resourceId) {
-        Resource foundResource = resourceService.findOne(resourceId);
-        return ResponseEntity.status(HttpStatus.OK).body(resourceMapper.resourceToDto(foundResource));
+        ResourceDto foundResource = resourceService.findOne(resourceId);
+        return ResponseEntity.status(HttpStatus.OK).body(foundResource);
     }
 
     @PostMapping
     public ResponseEntity<ResourceDto> createResource(@Valid @RequestBody ResourceDto resourceDto) {
-        Resource savedResource = resourceService.save(resourceMapper.dtoToResource(resourceDto));
-        return ResponseEntity.status(HttpStatus.CREATED).body(resourceMapper.resourceToDto(savedResource));
+        ResourceDto savedResource = resourceService.save(resourceDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedResource);
     }
 
     @PutMapping("/{id}")
@@ -63,8 +47,8 @@ public class ResourceController {
             @PathVariable("id") UUID resourceId,
             @Valid @RequestBody ResourceDto resourceDto
     ) {
-        Resource updatedResource = resourceService.update(resourceId, resourceMapper.dtoToResource(resourceDto));
-        return ResponseEntity.status(HttpStatus.CREATED).body(resourceMapper.resourceToDto(updatedResource));
+        ResourceDto updatedResource = resourceService.update(resourceId, resourceDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(updatedResource);
     }
 
     @DeleteMapping("/{id}")
