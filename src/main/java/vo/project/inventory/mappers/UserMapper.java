9b9ab10de8f2f26b0c5d20f39a952840e75ee3dd
Sophia Portela <sophianobreportela@gmail.com>
2025-09-02 22:27:41 -0300

    fix: remove unused import

--- src/main/java/vo/project/inventory/mappers/UserMapper.java
+++ src/main/java/vo/project/inventory/mappers/UserMapper.java
@@ -3,7 +3,6 @@ package vo.project.inventory.mappers;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import vo.project.inventory.domain.User;
-import vo.project.inventory.dtos.user.RegisterDto;
 import vo.project.inventory.dtos.user.UserDto;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
