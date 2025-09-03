30a82f9868d66ec96a8569ca01af16e1d8f3ab25
Sophia Portela <sophianobreportela@gmail.com>
2025-09-03 12:33:45 -0300

    fix: fix misspelling

--- src/main/java/vo/project/inventory/security/services/AuthTokenService.java
+++ src/main/java/vo/project/inventory/security/services/AuthTokenService.java
@@ -15,7 +15,7 @@ import java.time.ZoneOffset;
 @Service
 public class AuthTokenService {
 
-    @Value("${api.security.token.secret")
+    @Value("${api.security.token.secret}")
     private String secret;
 
     public String generateToken(UserDetailsImpl user) {
