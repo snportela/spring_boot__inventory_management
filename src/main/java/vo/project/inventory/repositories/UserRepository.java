6d6a2a168ffdbd26a43de41a00bd1beaee29b70a
Sophia Portela <sophianobreportela@gmail.com>
2025-09-02 22:24:31 -0300

    feat: create User repository

---
+++ src/main/java/vo/project/inventory/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package vo.project.inventory.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import vo.project.inventory.domain.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByResetToken(String resetToken);
+}
